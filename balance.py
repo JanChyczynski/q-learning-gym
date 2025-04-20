12b2cc4117fd6795d04fe7ab19cb00542ee3172c
jchyczynski <jasiek.ch16@gmail.com>
2025-04-20 16:11:03 +0200

    discretization fixed, basic qlearing finished

--- balance.py
+++ balance.py
@@ -1,10 +1,17 @@
 import math
+from random import random
+from typing import Tuple
 
 import gym
 import time
 
 class QLearner:
-    def __init__(self):
+    def __init__(self, learning_rate, discount_factor, experiment_rate, discretization_buckets):
+        self.learning_rate = learning_rate
+        self.discount_factor = discount_factor
+        self.experiment_rate = experiment_rate
+        self.discretization_buckets = discretization_buckets
+        self.default_q = 0
         self.environment = gym.make("CartPole-v1", render_mode="human")
         self.attempt_no = 1
         self.upper_bounds = [
@@ -19,7 +26,7 @@ class QLearner:
             self.environment.observation_space.low[2],
             -math.radians(50)
         ]
-        self.discretization_buckets = 4
+        self.q_dict = {}
 
     def learn(self, max_attempts):
         for _ in range(max_attempts):
@@ -31,8 +38,8 @@ class QLearner:
         done = False
         reward_sum = 0.0
         while not done:
-            self.environment.render()
-            action = self.picsk_action(observation)
+            # self.environment.render()
+            action = self.pick_action(observation)
             new_observation, reward, done, truncated, info = self.environment.step(action)
 
             new_observation = self.discretise(new_observation)
@@ -43,18 +50,37 @@ class QLearner:
         return reward_sum
 
     def discretise(self, observation):
-        return [round((val - low)/(up-low) * self.discretization_buckets)
+        return [round((val - low)/(up-low) * (self.discretization_buckets-1))
                 for val, low, up in zip(observation, self.lower_bounds, self.upper_bounds)]
 
     def pick_action(self, observation):
-        return self.environment.action_space.sample()
+        if random() > self.experiment_rate:
+            best_action, _ = self.best_action(observation)
+            return best_action
+        else:
+            return self.environment.action_space.sample()
+
+    def best_action(self, observation):
+        best_action = None
+        best_q = self.default_q
+        for action in range(self.environment.action_space.n):
+            current_q = self.q_dict.get((tuple(observation), action), self.default_q)
+            if best_action is None or current_q > best_q or (current_q == best_q and random() > 0.5):
+                best_action = action
+                best_q = current_q
+        assert best_action is not None
+        return best_action, best_q
 
     def update_knowledge(self, action, observation, new_observation, reward):
-        pass
-
+        new_q = reward + self.discount_factor * self.best_action(new_observation)[1]
+        if (tuple(observation), action) in self.q_dict.keys():
+            old_q = self.q_dict.get((tuple(observation), action), self.default_q)
+            self.q_dict[(tuple(observation), action)] = (1-self.learning_rate) * old_q + self.learning_rate * new_q
+        else:
+            self.q_dict[(tuple(observation), action)] = new_q
 
 def main():
-    learner = QLearner()
+    learner = QLearner(0.5, 0.5, 0.5, 4)
     learner.learn(10000)
 
 
